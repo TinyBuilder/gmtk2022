70a73aadacdbb2162a90a4ce760a950d73615af3
ivan <ivan.fernando.w@gmail.com>
2022-07-16 21:17:57 +0100

    Fixed turn taking procedures

--- Field.gd
+++ Field.gd
@@ -21,9 +21,12 @@ var is_rolling = false
 var active_dice
 var move_step = 0
 var last_dice_placement
+var possible_rays = []
+var visited_cells = []
 var Castle = preload("res://Castle.tscn")
 var FilledCell = preload("res://FilledCell.tscn")
 var castles = []
+var fillcount = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -31,9 +34,11 @@ func _ready():
 	for x in range(board_width):
 		board_state.append([])
 		cell_fills.append([])
+		visited_cells.append([])
 		for _y in range(board_height):
 			board_state[x].append(0)
 			cell_fills[x].append(null)
+			visited_cells[x].append(false)
 			# $TileMap.set_cellv(Vector2(x,y), 0)
 
 	var castle_coords = []
@@ -64,7 +69,10 @@ func _ready():
 
 
 func fill_cell(cell):
-	board_state[cell.x][cell.y] = turn
+	if board_state[cell.x][cell.y] > 2:
+		board_state[cell.x][cell.y] = turn + 3
+	else:
+		board_state[cell.x][cell.y] = turn
 	if cell_fills[cell.x][cell.y] == null:
 		cell_fills[cell.x][cell.y] = FilledCell.instance()
 		cell_fills[cell.x][cell.y].global_position = ($TileMap.map_to_world(cell))
@@ -107,53 +115,62 @@ func get_neighbours(cell):
 		if $TileMap.get_cellv(neighbour) >= 0:
 			neighbours.append(neighbour)
 
+	# print(neighbours)
+
 	return neighbours
 
 
-func is_enclosed(start, visited):
+func is_enclosed(start):
 	var enclosed = true
+	visited_cells[start.x][start.y] = true
 	for cardinal in get_cardinals(start):
 		if $TileMap.get_cellv(cardinal) < 0:
 			return false
 
-		if not visited[cardinal.x][cardinal.y]:
-			visited[cardinal.x][cardinal.y] = true
-			enclosed = enclosed and is_enclosed(cardinal, visited)
+		if not visited_cells[cardinal.x][cardinal.y]:
+			enclosed = enclosed and is_enclosed(cardinal)
 
 	return enclosed
 
 
 func flood_fill(start):
-	if board_state[start.x][start.y] > 2:
-		board_state[start.x][start.y] = turn + 3
-	else:
-		board_state[start.x][start.y] = turn
-
+	print("Flooding ", start)
+	
+	# var t = Timer.new()
+	# t.set_wait_time(1)
+	# t.set_one_shot(true)
+	# self.add_child(t)
+	# t.start()
+	# yield(t, "timeout")
+	
 	fill_cell(start)
 
 	for cardinal in get_cardinals(start):
+		if $TileMap.get_cellv(cardinal) < 0:
+			continue
+
 		if (
-			board_state[cardinal.x][cardinal.y] != turn
-			or board_state[cardinal.x][cardinal.y] != turn + 3
+			board_state[cardinal.x][cardinal.y] == turn
+			or board_state[cardinal.x][cardinal.y] == turn + 3
 		):
-			if $TileMap.get_cellv(cardinal) >= 0:
-				flood_fill(cardinal)
+			continue
+
+		flood_fill(cardinal)
 
 
 func detect_enclosed_spaces(cell):
 	var neighbours = get_neighbours(cell)
-	var visited_cells = []
-	for x in range(board_width):
-		visited_cells.append([])
-		for y in range(board_height):
-			var visited = false
-			if board_state[x][y] == turn or board_state[x][y] == turn + 3:
-				visited = true
-			visited_cells[x].append(visited)
-
+	
 	for neighbour in neighbours:
+		for x in range(board_width):
+			for y in range(board_height):
+				var visited = false
+				if board_state[x][y] == turn or board_state[x][y] == turn + 3:
+					visited = true
+				visited_cells[x][y] = visited
 		if not visited_cells[neighbour.x][neighbour.y]:
-			if is_enclosed(neighbour, visited_cells):
+			if is_enclosed(neighbour):
+				print("Enclosure found at: ", neighbour)
 				flood_fill(neighbour)
 
 
@@ -164,16 +181,32 @@ func _input(event):
 			var cell = $TileMap.world_to_map($TileMap.to_local(pos))
 			if $TileMap.get_cellv(cell) < 0 or board_state[cell.x][cell.y] > 0:
 				return
+			$Highlight.hide()
 			var cell_pos = $TileMap.map_to_world(cell) + $TileMap.global_position
-			if $TileMap.get_cellv(cell) >= 0 and board_state[cell.x][cell.y] == 0:
-				$Dice.global_position = cell_pos
-				$Dice.global_position = $Dice.global_position + Vector2(16, 16)
+			last_dice_placement = cell
+			$Dice.global_position = cell_pos + Vector2(16, 16)
 			fill_cell(cell)
 			detect_enclosed_spaces(cell)
+			$ArrowContainer.global_position = cell_pos + Vector2(16, 16)
+			$ArrowContainer.show()
+			var directions = get_adjacent(cell)
+			print("Surrounding cells: ", directions)
+			var valid_directions = 8
+			for i in range(8):
+				# print("Checking ", directions[i], board_state[directions[i].x][directions[i].y])
+				if (
+					$TileMap.get_cellv(directions[i]) < 0
+					or board_state[directions[i].x][directions[i].y] > 2
+				):
+					print(directions[i], " invalid")
+					valid_directions -= 1
+					$ArrowContainer.get_children()[i].hide()
+			if valid_directions < remaining_paths:
+				remaining_paths = valid_directions
 			move_step = 2
 
 		if not is_rolling and move_step == 0:
-			print("click")
+			# print("click")
 			dice_roll()
 
 
@@ -214,3 +247,88 @@ func _on_DiceRoller_hide():
 	remaining_paths = $DiceRoller.result
 	$Dice.set_face($DiceRoller.result)
 	$Dice.show()
+	is_rolling = false
+
+
+func choose_direction(event, direction):
+	if not event is InputEventMouseButton:
+		return
+	if not event.pressed:
+		return
+
+	for i in range(1, 8 - $DiceRoller.result):
+		# print(i)
+		var cell
+		match direction:
+			1:
+				cell = last_dice_placement + Vector2(0, -i)
+			2:
+				cell = last_dice_placement + Vector2(i, -i)
+			3:
+				cell = last_dice_placement + Vector2(i, 0)
+			4:
+				cell = last_dice_placement + Vector2(i, i)
+			5:
+				cell = last_dice_placement + Vector2(0, i)
+			6:
+				cell = last_dice_placement + Vector2(-i, i)
+			7:
+				cell = last_dice_placement + Vector2(-i, 0)
+			8:
+				cell = last_dice_placement + Vector2(-i, -i)
+
+		if $TileMap.get_cellv(cell) < 0 or board_state[cell.x][cell.y] > 2:
+			break
+
+		# print(cell)
+		fill_cell(cell)
+		detect_enclosed_spaces(cell)
+
+	$ArrowContainer.get_children()[direction - 1].hide()
+	remaining_paths -= 1
+	if remaining_paths < 1:
+		for arrow in $ArrowContainer.get_children():
+			arrow.show()
+		$ArrowContainer.hide()
+
+		$FillTimer.start()
+		yield($FillTimer, "timeout")
+
+		if turn == 1:
+			turn = 2
+		else:
+			turn = 1
+
+		move_step = 0
+
+
+func _on_Area2D_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 1)
+
+
+func _on_Area2D2_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 2)
+
+
+func _on_Area2D3_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 3)
+
+
+func _on_Area2D4_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 4)
+
+
+func _on_Area2D5_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 5)
+
+
+func _on_Area2D6_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 6)
+
+
+func _on_Area2D7_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 7)
+
+
+func _on_Area2D8_input_event(_viewport, event, _shape_idx):
+	choose_direction(event, 8)
